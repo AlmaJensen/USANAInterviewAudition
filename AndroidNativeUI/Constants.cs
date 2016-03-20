fdd1cd8ba86f13eab35923539982bd2b00b2a798
AlmaJensen <jazzdevstudio@gmail.com>
2016-03-19 22:21:56 -0600

    Added Constants File
    Added a file for Constant values.

---
+++ AndroidNativeUI/Constants.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidNativeUI
+{
+	public class Constants
+	{
+		public const string FeedURL = "http://feeds.feedburner.com/androidcentral?format=xml";
+	}
+}
